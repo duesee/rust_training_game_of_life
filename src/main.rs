af3da746042aeb03750b13dfe93cb960eb22c1c7
Damian Poddebniak <damian.poddebniak@fh-muenster.de>
2016-02-14 04:52:26 +0100

    Hmm... not satisfied with it yet...

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,32 @@
 extern crate rand;
 
 use rand::distributions::{IndependentSample, Range};
-use rand::Rng;
+use std::fmt::{self, Display, Formatter};
 use std::thread::sleep;
 use std::time::Duration;
 
-enum BufferLocation {Foreground, Background}
+enum BufferLocation { Foreground, Background }
 
-struct Gol {
+struct Board {
     rows:     usize,
     cols:     usize,
+    // We use two buffers: During calculation, the foreground buffer is
+    // read-only and changes to the cells are written to the background
+    // buffer. When a calculation has finished, the buffers are swapped,
+    // so that the fg-buffer becomes the bg-buffer and vice versa.
     switch:   bool,
     buffer_a: Vec<Vec<bool>>,
     buffer_b: Vec<Vec<bool>>,
 }
 
-impl Gol {
-    fn new(rows: usize, cols: usize) -> Gol {
-        // create a 2-dimensional field with a phantom frame
+impl Board {
+    fn new(rows: usize, cols: usize) -> Board {
+        // Allocate two more rows and cols to construct an always dead
+        // phantom border. This simplifies the next-gen algorithm.
         let buffer_a = vec![vec![false; cols + 2]; rows + 2];
-        let buffer_b = buffer_a.clone();
+        let buffer_b = vec![vec![false; cols + 2]; rows + 2];
 
-        Gol {
+        Board {
             rows:     rows,
             cols:     cols,
             switch:   true,
@@ -30,57 +35,47 @@ impl Gol {
         }
     }
 
-    fn init(&mut self) {
+    fn populate(&mut self) {
         let (rows, cols) = (self.rows, self.cols);
         let mut buffer   = self.get_buffer(BufferLocation::Foreground);
-        
         let mut rng      = rand::thread_rng();
-        let between      = Range::new(0, 100);
+        let between      = rand::distributions::Range::new(0, 100);
 
         for row in 1..rows + 1 {
             for col in 1..cols + 1 {
-                buffer[row][col] = between.ind_sample(&mut rng) < 5;
+                buffer[row][col] = between.ind_sample(&mut rng) < 7;
             }
         }
     }
 
-    fn print(&mut self) {
-        let (rows, cols) = (self.rows, self.cols);
-        let buffer_fg    = self.get_buffer(BufferLocation::Foreground);
-
-        for row in 1..rows + 1 {
-            for col in 1..cols + 1 {
-                if buffer_fg[row][col] { print!("•") } else { print!(" ") };
-            }
-            println!("");
-        }
-    }
-  
     fn next(&mut self) {
         {
             let (rows, cols)  = (self.rows, self.cols);
-
-            // TODO: We need an mutable handle to buffer_bg and an immutable handle to buffer_fg.
-            // This is not possible due to the borrowing policies. What is an idiomatic way of doing this?
             let     buffer_fg = self.get_buffer(BufferLocation::Foreground).clone();
             let mut buffer_bg = self.get_buffer(BufferLocation::Background);
 
             for row in 1..rows + 1 {
                 for col in 1..cols + 1 {
+                    // Here is another small simplification of the algorithm:
+                    // Instead of starting the counter with zero, start with
+                    // minus 1 ...
                     let mut counter = -1;
                     for row_d in 0..3 {
                         for col_d in 0..3 {
-                            if buffer_fg[row + row_d - 1][col + col_d - 1] {
+                            if buffer_fg[row + row_d - 1]
+                                        [col + col_d - 1] {
                                 counter += 1;
                             }
                         }
                     }
+                    // ... and adapt the rules accordingly. 
                     buffer_bg[row][col] = counter == 2 || counter == 3;
                 }
             }
         }
-
-        self.switch_buffer();
+        
+        // Don't forget to swap the buffers.
+        self.switch = !self.switch;
     }
 
     fn get_buffer(&mut self, loc: BufferLocation) -> &mut Vec<Vec<bool>> {
@@ -99,22 +94,39 @@ impl Gol {
             }
         }
     }
+}
 
-    fn switch_buffer(&mut self) {
-        self.switch = !self.switch;
+impl Display for Board {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let (rows, cols) = (self.rows, self.cols);
+        let buffer_fg    = match self.switch {
+            true  => &self.buffer_a,
+            false => &self.buffer_b
+        };
+
+        for row in 1..rows + 1 {
+            for col in 1..cols + 1 {
+                if buffer_fg[row][col] {
+                    try!(write!(f, "•"))
+                } else {
+                    try!(write!(f, " "))
+                };
+            }
+            try!(writeln!(f, ""));
+        }
+        
+        Ok(())
     }
 }
 
 fn main() {
-    let mut gol = Gol::new(40, 60);
+    let mut board = Board::new(40, 60);
     
-    gol.init();
+    board.populate();
 
-    loop {
-        println!("------------------------------------------------------------");
-        gol.print();
-        println!("------------------------------------------------------------");
-        gol.next();
-        sleep(Duration::from_millis(100));
+    for cnt in 1.. {
+        board.next();
+        println!("{}\n{:^60}", board, format!("<<< Generation: {:>6} >>>", cnt));
+        sleep(Duration::from_millis(150));
     }
 }
